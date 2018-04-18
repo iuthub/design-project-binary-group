211bbc7e7d20f89dd7deac003bbb48223fc3924a
ikhumoyun <khumoyun@inoyatov.com>
2018-04-18 05:23:19 +0500

    [Gulpfile.js] Initial configs

---
+++ frontend/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+
+gulp.task('default', function() {
+    return gulp.src('src/entry.js')
+        .pipe(webpack({
+            
+        }))
+        .pipe(gulp.dest('dist/'));
+});
