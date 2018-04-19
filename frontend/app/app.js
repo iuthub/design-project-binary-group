916b875e63fe54e713e2533a755f1e2da13bcf8b
ikhumoyun <khumoyun@inoyatov.com>
2018-04-19 17:16:48 +0500

    Gulp [Babel and SASS] initial configs

---
+++ frontend/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from React;
+
+class App extends React.Component {
+    render() {
+        return {
+
+        }
+    }
+}
+
+export App default
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const webpack = require('webpack-stream');
+
+
+gulp.task('translate', function() {
+    return gulp.src('app/**/*.js')
+        .pipe(babel({
+            presets: ['es2015', 'react']
+        }))
+        .pipe(gulp.dest('/dist'));
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('/dist'));
+})
 
 gulp.task('default', function() {
     return gulp.src('src/entry.js')
